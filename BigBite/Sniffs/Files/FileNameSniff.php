45fb487d6462f51b1fda91ca5085c9498ff7e508
Jon McPartland <jon@bigbite.net>
2022-04-14 14:09:51 +0100

    Add Sniffing support for interfaces and traits

--- BigBite/Sniffs/Files/FileNameSniff.php
+++ BigBite/Sniffs/Files/FileNameSniff.php
@@ -59,36 +59,13 @@ class FileNameSniff extends WPFileNameSniff {
 
 		unset( $expected );
 
-		/*
-		 * Check files containing a class for the "class-" prefix and that the rest of
-		 * the file name reflects the class name. Accounts for abstract classes.
-		 */
-		if ( true === $this->strict_class_file_names ) {
-			$has_class = $this->phpcsFile->findNext( \T_CLASS, $stackPtr );
-
-			if ( false !== $has_class && false === $this->is_test_class( $has_class ) ) {
-				$is_abstract = $this->phpcsFile->findPrevious( \T_ABSTRACT, $has_class );
-				$class_name  = $this->phpcsFile->getDeclarationName( $has_class );
-
-				if ( is_null( $class_name ) ) {
-					$class_name = 'anonymous';
-				}
-
-				$expected    = 'class-' . $this->kebab( $class_name );
-				$err_message = 'Class file names should be based on the class name with "class-" prepended. Expected %s, but found %s.';
-
-				if ( $is_abstract ) {
-					$expected    = 'abstract-' . $expected;
-					$err_message = 'Abstract class file names should be based on the class name with "abstract-class-" prepended. Expected %s, but found %s.';
-				}
-
-				if ( substr( $fileName, 0, -4 ) !== $expected ) {
-					$this->phpcsFile->addError( $err_message, 0, 'InvalidClassFileName', [ $expected . '.php', $fileName ] );
-				}
-
-				unset( $expected );
-			}
+		if ( true !== $this->strict_class_file_names ) {
+			return ( $this->phpcsFile->numTokens + 1 );
 		}
+
+		$this->check_maybe_class( $stackPtr, $fileName );
+		$this->check_maybe_trait( $stackPtr, $fileName );
+		$this->check_maybe_interface( $stackPtr, $fileName );
 
 		// Only run this sniff once per file, no need to run it again.
 		return ( $this->phpcsFile->numTokens + 1 );
@@ -127,6 +104,99 @@ class FileNameSniff extends WPFileNameSniff {
 		}
 
 		return false;
+	}
+
+	/**
+	 * Check files containing a class for the "class-" prefix,
+	 * and that the rest of the file name reflects the class name.
+	 * Accounts for abstract classes.
+	 *
+	 * @param mixed  $stackPtr the token stack
+	 * @param string $fileName the name of the file
+	 *
+	 * @return void
+	 */
+	protected function check_maybe_class( $stackPtr, $fileName ) {
+		$has_class = $this->phpcsFile->findNext( \T_CLASS, $stackPtr );
+
+		if ( false === $has_class || false !== $this->is_test_class( $has_class ) ) {
+			return;
+		}
+
+		$is_abstract = $this->phpcsFile->findPrevious( \T_ABSTRACT, $has_class );
+		$class_name  = $this->phpcsFile->getDeclarationName( $has_class );
+
+		if ( is_null( $class_name ) ) {
+			$class_name = 'anonymous';
+		}
+
+		$expected    = 'class-' . $this->kebab( $class_name );
+		$err_message = 'Class file names should be based on the class name with "class-" prepended. Expected %s, but found %s.';
+
+		if ( $is_abstract ) {
+			$expected    = 'abstract-' . $expected;
+			$err_message = 'Abstract class file names should be based on the class name with "abstract-class-" prepended. Expected %s, but found %s.';
+		}
+
+		if ( substr( $fileName, 0, -4 ) === $expected ) {
+			return;
+		}
+
+		$this->phpcsFile->addError( $err_message, 0, 'InvalidClassFileName', [ $expected . '.php', $fileName ] );
+	}
+
+	/**
+	 * Check files containing a trait for the "trait-" prefix,
+	 * and that the rest of the file name reflects the trait name.
+	 *
+	 * @param mixed  $stackPtr the token stack
+	 * @param string $fileName the name of the file
+	 *
+	 * @return void
+	 */
+	protected function check_maybe_trait( $stackPtr, $fileName ) {
+		$has_trait = $this->phpcsFile->findNext( \T_TRAIT, $stackPtr );
+
+		if ( false === $has_trait || false !== $this->is_test_class( $has_trait ) ) {
+			return;
+		}
+
+		$trait_name  = $this->phpcsFile->getDeclarationName( $has_trait );
+		$expected    = 'trait-' . $this->kebab( $trait_name );
+		$err_message = 'Trait file names should be based on the class name with "trait-" prepended. Expected %s, but found %s.';
+
+		if ( substr( $fileName, 0, -4 ) === $expected ) {
+			return;
+		}
+
+		$this->phpcsFile->addError( $err_message, 0, 'InvalidTraitFileName', [ $expected . '.php', $fileName ] );
+	}
+
+	/**
+	 * Check files containing an interface for the "interface-" prefix,
+	 * and that the rest of the file name reflects the interface name.
+	 *
+	 * @param mixed  $stackPtr the token stack
+	 * @param string $fileName the name of the file
+	 *
+	 * @return void
+	 */
+	protected function check_maybe_interface( $stackPtr, $fileName ) {
+		$has_interface = $this->phpcsFile->findNext( \T_INTERFACE, $stackPtr );
+
+		if ( false === $has_interface || false !== $this->is_test_class( $has_interface ) ) {
+			return;
+		}
+
+		$interface_name = $this->phpcsFile->getDeclarationName( $has_interface );
+		$expected       = 'interface-' . $this->kebab( $interface_name );
+		$err_message    = 'Interface file names should be based on the interface name with "interface-" prepended. Expected %s, but found %s.';
+
+		if ( substr( $fileName, 0, -4 ) === $expected ) {
+			return;
+		}
+
+		$this->phpcsFile->addError( $err_message, 0, 'InvalidInterfaceFileName', [ $expected . '.php', $fileName ] );
 	}
 
 	/**
