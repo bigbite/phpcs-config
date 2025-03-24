346a2ae6857600756285e4255c8c558fa1777c7a
Jay McPartland <jay@bigbite.net>
2025-03-24 09:38:29 +0000

    Implement new Sniff to ensure a blank line before an object's closing brace

---
+++ BigBite/Tests/Objects/NewLineBeforeClosingBraceUnitTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Unit test class for BigBite Coding Standard.
+ *
+ * @package BigBiteCS\BigBite
+ * @link    https://github.com/bigbite/phpcs-config
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+namespace BigBiteCS\BigBite\Tests\Objects;
+
+use BigBiteCS\BigBite\Tests\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the NewLineBeforeClosingBrace sniff.
+ *
+ * @package BigBiteCS\BigBite
+ */
+final class NewLineBeforeClosingBraceUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * A list of tokenizers this sniff supports.
+	 *
+	 * @var array<int,string>
+	 */
+	public $supportedTokenizers = array(
+		'PHP',
+	);
+
+	/**
+	 * Returns an array of tokens this test wants to listen for.
+	 *
+	 * @return array<int,int|string>
+	 */
+	public function register() {
+		return array(
+			\T_CLASS,
+			\T_ENUM,
+			\T_INTERFACE,
+			\T_TRAIT,
+		);
+	}
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of errors that should occur on that line.
+	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
+	 * @return array<int,int>
+	 */
+	public function getErrorList( $testFile = '' ) {
+		switch ( $testFile ) {
+			case 'NewLineBeforeClosingBraceUnitTest.1.inc':
+				return array(
+					6  => 1,
+					12 => 1,
+					16 => 1,
+					20 => 1,
+				);
+			default:
+				return array();
+		}
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * The key of the array should represent the line number and the value
+	 * should represent the number of warnings that should occur on that line.
+	 *
+	 * @param string $testFile The name of the file being tested.
+	 *
+	 * @return array<int,int>
+	 */
+	public function getWarningList( $testFile = '' ) {
+		return array();
+	}
+}
